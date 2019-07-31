619e5c4cbb54598b07b475cb588902e6bb234048
shadowdreamer <uzumaki@yeah.net>
2019-07-31 17:31:01 +0800

    Update BottomSheet.vue

--- src/components/BottomSheet.vue
+++ src/components/BottomSheet.vue
@@ -2,14 +2,15 @@
   <v-bottom-sheet :value="value" persistent>
     <v-sheet class="text-center pa-4" height="200px">
       <v-progress-circular
+        :indeterminate="message=='pending'"
         :rotate="360"
         :size="100"
         :width="15"
         :value="progress"
-        color="teal"
+        color="primary"
       >{{ progress }}%</v-progress-circular>
-    </v-sheet>
     <div>{{message}}</div>
+    </v-sheet>
   </v-bottom-sheet>
 </template>
 <script>
@@ -20,7 +21,10 @@ export default {
       default: 0
     },
     value: Boolean,
-    message: String
+    message: {
+      type:String,
+      default:''
+    }
   }
 }
 </script>
