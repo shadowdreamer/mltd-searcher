fb58d2a9e9f716359c9d89e527161e31b94eaf81
fireattack <human.peng@gmail.com>
2019-08-07 02:38:57 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,11 +3,11 @@
     <v-card-text>
       <div class="headline black--text font-weight-regular mb-5">About this site</div>
       <div class="filter-divider">
-        <hr /> <div>DATA PROVIDER</div> <hr />
+        <hr /> <div>Data Provider</div> <hr />
       </div>
       <div class="subtitle-1">
         <p>Thanks to</p>
-        <span class="princess">P</span>ublic<span class="princess">R</span>ESTful Application Programming<span class="princess"> In</span>terfa<span class="princess">ce</span><span class="princess"> S</span>erie<span class="princess">s</span>.
+        <span class="princess">P</span>ublic <span class="princess">R</span>ESTful Application Programming<span class="princess"> In</span>terfa<span class="princess">ce</span><span class="princess"> S</span>erie<span class="princess">s</span>.
       </div>
       <div class="text-center mb-5">
         <v-btn color="#ea5b76" dark target="_blank" 
@@ -19,7 +19,7 @@
       <div class="filter-divider">
         <hr /> <div>Source Code</div> <hr />
       </div>
-        <p>Build by vue and vuetify,view more at github:</p>
+        <p>Built by vue and vuetify, view more at GitHub:</p>
       <div class="text-center mb-5">
         <v-btn  color="#1a1a1a" dark target="_blank" 
         href="https://github.com/shadowdreamer/mltd-searcher">github
@@ -27,11 +27,11 @@
         </v-btn>
       </div>
       <div class="filter-divider mt-5">
-        <hr /> <div>copyright</div> <hr />
+        <hr /> <div>Copyright</div> <hr />
       </div>
        The copyright to THE IDOLM@STER contents belongs to BANDAI NAMCO Entertainment Inc.
       <div class="filter-divider mt-5">
-        <hr /> <div>licenses</div> <hr />
+        <hr /> <div>License</div> <hr />
       </div>
       <v-btn small text href="https://opensource.org/licenses/MIT"  target="_blank">MIT 
       <v-icon class="mdi" small>mdi-open-in-new</v-icon></v-btn>
